b81f06d767f9d5c25cb86558d044d8b58385c590
CoderGuy614 <jlutz.110@gmail.com>
2020-05-05 15:02:14 +0800

    Get a single post action and post detail component added

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import PrivateRoute from "./components/routing/PrivateRoute";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 
 //Redux
@@ -67,6 +68,7 @@ const App = () => {
                 component={AddEducation}
               />
               <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
             </Switch>
           </section>
         </Fragment>
